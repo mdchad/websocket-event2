27de2bbc0e468fca3377cfd408fc4ea5d677331d
Irsyad <irsyad.muhd@gmail.com>
2016-11-14 00:02:18 +0800

    fix layout bug

--- public/map.js
+++ public/map.js
@@ -16,7 +16,6 @@ $(function () {
 
   $messageForm.submit(function (e) {
     e.preventDefault()
-    console.log('submitted')
     socket.emit('send message', $message.val())
     $message.val(' ')
   })
@@ -25,18 +24,20 @@ $(function () {
     $chat.append('<li><strong>' + data.user + '</strong>: ' + data.msg + '</li>')
   })
 
-  $pacInputForm.submit(function (e) {
-    e.preventDefault()
-    console.log()
-    socket.emit('send event', $pacInput.val())
-    $pacInput.val(' ')
-  })
+  // $pacInputForm.submit(function (e) {
+  //   e.preventDefault()
+  //   socket.emit('send event', $pacInput.val())
+  //   $pacInput.val(' ')
+  // })
 
 
 
   // submit username
   $userForm.submit(function (e) {
     e.preventDefault()
+    $('.content').show(500, function(){
+
+    })
     console.log('submitted')
     socket.emit('new user', $username.val(), function (data) {
       if (data) {
@@ -60,7 +61,7 @@ $(function () {
 /////////////////////////***MAP**//////////////////////
 $(document).ready(function () {
   // loading the base map
-  var mymap = L.map('mapid').setView([1.3481, 103.8208], 12)
+  var mymap = L.map('mapid').setView([1.3481, 103.8208], 12).invalidateSize(true)
 
   L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: '<a href="http://mapbox.com">Mapbox</a>',
@@ -69,9 +70,9 @@ $(document).ready(function () {
     accessToken: 'pk.eyJ1IjoiZGF2aWZpZWQiLCJhIjoiY2lxYWoxMnF3MDF0Z2Z2bTZ6MHl3cWdiMyJ9.JhNjMNWSTxbGzp7ck3ahMA'
   }).addTo(mymap)
 
-  $('button#submit').click(function() {
+  $('#searchBar').submit(function(e) {
+    e.preventDefault()
     var searchTerm = $('input#search').val()
-    console.log(searchTerm);
     socket.emit('search', searchTerm)
   })
 
@@ -79,78 +80,6 @@ $(document).ready(function () {
 
   socket.on('geocoded latlong', function(data) {
     var marker = L.marker([data.latlng.lat, data.latlng.lng]).addTo(mymap).bindPopup(data.user);
-    // console.log(data.latlng.lat);
-    // console.log(data.latlng.lng);
-
     $('ul#list').append('<li>' + data.place + '</li>')
   })
-
-
-  // $("#list").click(function(e) {
-  //   console.log("clicked")
-  //   if ($(this).html() == "Like") {
-  //       $(.like-Unlike).html('Unlike');
-  //   }
-  //   else {
-  //       $(this).html('Like');
-  //   }
-  //   return false;
-  // });
-
-
-  // making ajax request to our own api
-  // function getData () {
-  //   $.ajax({
-  //     url: 'https://carparks-sg.herokuapp.com/api',
-  //     // url: 'http://localhost:3000/api',
-  //     type: 'GET',
-  //     success: function (data) {
-  //       visualiseData(data)
-  //     }
-  //   })
-  // }
-  //
-  // // adding all circles to a layer so that we can remove and re-add the circles every time the API call is made (once per minute)
-  // var circlesGroup = new L.FeatureGroup()
-  //
-  // //  DEFINE FUNCTION FOR VISUALISING DATA RETURNED FROM AJAX CALL
-  // function visualiseData (data) {
-  //   data.sort(function (b, a) { return (a.Development > b.Development) ? 1 : ((b.Development > a.Development) ? -1 : 0) })
-  //
-  //   for (var i = 0; i < data.length; i++) {
-  //     var lon = data[i].Longitude
-  //     var lat = data[i].Latitude
-  //     var lots = data[i].Lots
-  //     var message = data[i].Development + ': ' + data[i].Lots + ' lots left'
-  //
-  //     // appending class properties so that we can manipulate each data point in the DOM
-  //     $('ul.dropdown-menu').prepend('<li class="carpark" name="' + data[i].Development + '" lon="' + data[i].Longitude + '" lat="' + data[i].Latitude + '" lots="' + data[i].Lots + '"><a href="#">' + data[i].Development + '</a></li>')
-  //
-  //     var intensity = lots / 600
-  //
-  //     if (lots > 50) {
-  //       var circle = L.circle([lat, lon], 80, {
-  //         fillColor: '#09AD83',
-  //         fillOpacity: intensity,
-  //         stroke: false,
-  //         className: 'animate'
-  //       })
-  //       circle.bindPopup(message)
-  //       circlesGroup.addLayer(circle)
-  //     } else {
-  //       circle = L.circle([lat, lon], 80, {
-  //         fillColor: '#FF6A59',
-  //         fillOpacity: 0.8,
-  //         stroke: false,
-  //         className: 'animate'
-  //       })
-  //       circle.bindPopup(message)
-  //       circlesGroup.addLayer(circle)
-  //     }
-  //   }
-  //   mymap.addLayer(circlesGroup)
-  //   setTimeout(function () {
-  //     mymap.removeLayer(circlesGroup)
-  //   }, 59500)
-  // }
 })
--- server.js
+++ server.js
@@ -46,16 +46,6 @@ io.sockets.on('connection', function (socket) {
     io.sockets.emit('get users', users)
   }
 
-  // Send event
-  // socket.on('send event', function (data) {
-  //   console.log(data)
-  //   io.sockets.emit('new event', {event: data})
-  // })
-  //
-  // socket.on('send icon', function (data) {
-  //   console.log('data send ', data)
-  //   io.sockets.emit('new icon', data)
-  // })
   socket.on('search', function (data) {
     geocoder.geocode(data, function (err, response) {
       console.log(response)
@@ -72,29 +62,3 @@ io.sockets.on('connection', function (socket) {
     })
   })
 })
-
-// io.sockets.on('connection', function (socket) {
-//   connections.push(socket)
-//   console.log('Connected: %s sockets connected', connections.length)
-//
-//   socket.on('disconnect', function (data) {
-//     users.splice(users.indexOf(socket.username), 1)
-//     // updateUsernames()
-//     connections.splice(connections.indexOf(socket), 1)
-//     console.log('Disconnected: %s sockets connected', connections.length)
-//   })
-//
-//   socket.on('search', function (data) {
-//     geocoder.geocode(data, function (err, response) {
-//       console.log(response);
-//       if (response.results[0].geometry['bounds'] !== undefined ) {
-//         latlong = response.results[0].geometry['bounds']['northeast']
-//       } else if (response.results[0].geometry['bounds'] === undefined ){
-//         // console.log(response.results[0].geometry['location'])
-//         latlong = response.results[0].geometry['location']
-//       }
-//       var searchTerm = {place: data, latlng: latlong}
-//       io.sockets.emit('geocoded latlong', searchTerm)
-//     })
-//   })
-// })
